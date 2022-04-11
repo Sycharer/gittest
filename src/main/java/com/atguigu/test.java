cbb6d07b2f1fd3ca84685593d800927b58d9e378
Sycharer <82245786+Sycharer@users.noreply.github.com>
2022-04-11 23:18:31 +0800

    first Commit

---
+++ src/main/java/com/atguigu/test.java
@@ -0,0 +1,16 @@
+package com.atguigu;
+
+/**
+ * Created with Intellij IDEA.
+ *
+ * @Description:
+ * @Author: HazamaJadeBall  Email: 2562553446@qq.com
+ * @Date: 2022/4/11  23:17
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
